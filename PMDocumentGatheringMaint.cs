e5710b1bd0c937a73dbbf4e422a5609dd1a468ed
Paul Gilfoy <paul.gilfoy+github@gmail.com>
2023-02-27 17:01:01 -0500

    Update PMDocumentGatheringMaint.cs

--- PMDocumentGatheringMaint.cs
+++ PMDocumentGatheringMaint.cs
@@ -6,7 +6,7 @@ using PX.Data.BQL.Fluent;
 
 namespace ProjectTask
 {
-  public class PMDocumentGatheringMaint  : PXGraph<PMDocumentGatheringMaint, PMDocumentGathering>
+  public class PMDocGatheringMaint  : PXGraph<PMDocGatheringMaint, PMDocumentGathering>
   {
     
     public SelectFrom<PMDocumentGathering>.View DocumentGathering;
@@ -34,6 +34,127 @@ namespace ProjectTask
     public class DetailsTable : IBqlTable
     {
 
+    }
+
+
+    protected void PMDocumentGathering_Mpr_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.MPR_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+    
+    protected void PMDocumentGathering_PayrollVerification_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.PayrollVerification_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_Spd_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.SPD_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_Aa_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.AA_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+
+      
+    }
+
+    protected void PMDocumentGathering_Bpd_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.BPD_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_Irs_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.IRS_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_SATrustree_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.SATrustree_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_Satpa_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.SATPA_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_SAInvestment_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.SAInvestment_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+
+
+
+
+
+
+
+    protected void PMDocumentGathering_Bpd_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.BPD_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_Bpd_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.BPD_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_Bpd_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.BPD_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_Bpd_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.BPD_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
+    }
+
+    protected void PMDocumentGathering_Bpd_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    {
+      
+      var row = (PMDocumentGathering)e.Row;
+      row.BPD_LastModifiedDateTime = PX.Common.PXTimeZoneInfo.Now.Date;
+      
     }
 
 
